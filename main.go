f205d5cd263275932634f32fe0012ad699021a49
Admin <subashbaidya9@gmail.com>
2023-08-30 19:45:04 +0545

    Printing and Formatting Strings

--- main.go
+++ main.go
@@ -3,52 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	// strings 
-	// Type Annotation
-	var nameOne string = "Name"; //Implicitly typing a variable
-	// ! We cannot use single quotes for the string, we must use double quotes.
-	// !This does show error because we're declaring a variable but not using it.
-	fmt.Println(nameOne);
+	// Print
+	// This type of print doesn't add new line so the console print would be "hello, world"
+	fmt.Print("hello, ")
+	fmt.Print("world")
+	fmt.Print("\nNew Line\n") // This is how you can print new line.
 
-	// Type Inference	
-	var nameTwo = "Name Two";
-	fmt.Println(nameTwo);
+	// Println
+	// This would print these two statements in new lines.
+	fmt.Println("Hello")
+	fmt.Println("Duniya")
 
-	// Just declaration
-	var nameThree string;
-	
-	nameThree = "three"; // * If you don't have any value the console will just print blank
-	fmt.Println(nameThree);
+	// Printing variables using print function
+	age := 32
+	name := "naam"
 
-	// Short hand variable declaration
-	nameFour := "Name Four"; // * This is equivalent to all the declaration above
-	// ! You would use this only for the first time when you're declaring the variable but not when assigning value later on in the program.
-	// ! You cannot use this outside of the function as well.
-	fmt.Println(nameFour)
+	fmt.Println("my name is ", age)
+	fmt.Println("and my name is ", name)
 
-	// Numbers
-	// * There are two type of number types: Integer and Floats
-	// Integer
-	var ageOne int = 10;
-	var ageTwo  = 20;
-	var ageThree int;
-	ageThree = 30;
-	ageFour := 40;
+	// Formatted string
+	// * %v is the default format specifier
+	// There are different format specifier
+	fmt.Printf("My age is %v and my name is %v \n", age, name)
+	fmt.Printf("My age is %q and my name is %q \n", age, name) // %q puts the formatter into quotation
+	// ! This will not work for numbers and only string. Output: My age is ' '
 
-	fmt.Println(ageOne, ageTwo, ageThree, ageFour);
+	// %T will print the type of the variable.
+	fmt.Printf("Age is of type %T\n", age)
+	// * Output: Age is of type int
 
-	// bits and memory
-	var numOne int8 = 23; // Go allows us to set the range of ints like this in this case int8 ranges from -128 to 127
-	// ! You cannot have 128 assigned in this case. var numOne int8 = 128 
-	// There is concept of uint as well. Which doesn't allow negative number.
+	// Printing float
+	fmt.Printf("you scored %f points.\n", 22.3)
+	fmt.Printf("you scored %.2f points.\n", 22.3)
 
-	var posOne uint = 2; // !Cannot allow -2 here
-	fmt.Println(numOne, posOne)
-
-	// float
-	var scoreOne float32 = 1.5;
-	var scoreTwo float64 = 1982739840701837410987908701928374901865897346509812394.128798341098374;
-	scoreThree := 64.64; // * This will be inferred as default float64
-	fmt.Println(scoreOne, scoreTwo, scoreThree) 
+	// Sprintf: Saves the formatted string
+	var str = fmt.Sprintf("My age is %q and my name is %q \n", age, name)
+	fmt.Printf("The saved String is: %v", str)
 
 }
