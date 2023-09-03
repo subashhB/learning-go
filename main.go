14b32e7b094b6801f148602ed9dd88e13001d61f
Admin <subashbaidya9@gmail.com>
2023-09-03 17:21:24 +0545

    Using function

--- main.go
+++ main.go
@@ -1,39 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(name string) {
+	fmt.Println("Good Morning", name)
+}
+
+func sayBye(name string) {
+	fmt.Println("Good Bye", name)
+}
+
+func cycleNames(names []string, function func(string)) {
+	for _, v := range names {
+		function(v)
+	}
+}
+
+func cirleArea(radius float64) float64 {
+	return math.Pi * radius * radius
+}
 
 func main() {
-	age := 45
+	sayGreeting("Zelda")
+	sayGreeting("Tifa")
+	sayBye("Zelda")
 
-	// Booleans
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 45)
-	fmt.Println(age != 50)
+	cycleNames([]string{"Romulus", "Orion", "Merlin", "Solomon", "Hasan", "Noah"}, sayGreeting) //We aren't calling the function otherwise we would be invoking the function here only which we don't want instead we are passing the reference of the function.
+	cycleNames([]string{"Romulus", "Orion", "Merlin", "Solomon", "Hasan", "Noah"}, sayBye)
 
-	if age <= 30 {
-		fmt.Println("age is less than or equal to 30")
-	} else if age <= 40 {
-		fmt.Println("age is less than or equal to 40")
-	} else {
-		fmt.Println("age is less than or equal to neither 30 nor 40")
-	}
-	grandServants := []string{"Romulus=Quirinus", "Orion", "Merlin", "Hassan of the Mountains", "Solomon", "Noah"}
-
-	for index, value := range grandServants {
-		if index == 1 {
-			fmt.Println("Continuing at pos ", index)
-			continue //This will break the iteration but will continue the loop by taking the process to the start of the loop but will stop the process form going further down to the codes below.
-			// @param 1 For index == 1, the print below iwll not be executed.
-		}
-
-		if index < 2 {
-			fmt.Println("Break at position ", index)
-			break //This breaks the loop entirely with no continue
-		}
-
-		fmt.Printf("The value at pos %v is %v\n", index, value)
-
-	}
+	area1 := cirleArea(7)
+	area51 := cirleArea(7.5)
+	fmt.Printf("Circle 1 is %.3f and Circle 2 is %.3f", area1, area51)
 
 }
