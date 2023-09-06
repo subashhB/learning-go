2b4609668c8e1f01e9ce034fcf469139f6fbff30
Admin <subashbaidya9@gmail.com>
2023-09-06 08:06:23 +0545

    Pass by value

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import "fmt"
 
+func updateName(name string) string {
+	name = "wedge"
+	return name //If we have to get this value then we return the value
+}
+
+func updateMenu(menuMap map[string]float64) {
+	menuMap["coffe"] = 29.9
+}
+
 func main() {
-	// menu is a key value pair.
-	// menu := map[key_datatype]value_datatype
+	// Non pointer values
+	name := "Tifa"
+
+	updatedName := updateName(name) //Here a copy of the value is sent not the actual value so when something happens inside the function it happens to the copy and not the actual variable.
+
+	fmt.Println(name)
+	fmt.Println(updatedName)
+
+	// Pointer Wrapper Values
 	menu := map[string]float64{
 		"soup":           44.9,
 		"pie":            79.9,
@@ -12,33 +28,10 @@ func main() {
 		"toffee pudding": 35.5,
 	}
 
+	updateMenu(menu) //In this case the original value does change for array, slice, maps, struct
+	// This happens so because in these datatypes Go splits the data into two memory blocks.
+	// One block contains the values and other contains "Pointer" pointing to the memory address to the values.
+	// When passed by value it copies the memory address itself so whatever the changes are done are reflected to the original values since the copy points to the same memory location.
 	fmt.Println(menu)
-	fmt.Println(menu["pie"])
-
-	// Looping maps
-	for k, v := range menu {
-		fmt.Println(k, "-", v)
-	}
-
-	// Int as key type
-	phonebook := map[int]string{
-		9841984198:  "Shyam",
-		98039830398: "Ram",
-		9843984398:  "Sita",
-	}
-
-	fmt.Println(phonebook)
-	fmt.Println(phonebook[9841984198])
-
-	// updating maps
-	phonebook[9843984398] = "Alice"
-	fmt.Println(phonebook)
-
-	phonebook[98039830398] = "Bob"
-	fmt.Println(phonebook)
-
-	// This add a new data into maps
-	phonebook[12345678] = "Alex"
-	fmt.Println(phonebook)
 
 }
