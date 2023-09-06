3c2cbd06bc114f8601baeb62ac2a3c8bd22721be
Admin <subashbaidya9@gmail.com>
2023-09-06 14:01:32 +0545

    Receiver Function

--- bill.go
+++ bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -10,9 +12,29 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{},
+		items: map[string]float64{"pie": 59.9, "cake": 139.6},
 		tip:   0,
 	}
 
 	return b
+}
+
+// Receiver Function: format the bill
+// Now we can access this function by billObject.format() but we cannot call format() solo.
+func (b bill) format() string {
+	formattedString := "Bill Breakdown: \n"
+
+	var total float64 = 0
+
+	// List items
+	for k, v := range b.items {
+		formattedString += fmt.Sprintf("%-25v ....Rs.%v \n", k+":", v) // -25 here makes the string 25 characters long, even if the value isn't 25 characters long, white blank spaces are added.
+		total += v
+	}
+
+	// Total formatting in string
+	formattedString += fmt.Sprintf("\n%-25v ....Rs.%.2f", "total:", total) // IF we would have put +25 here then the spacing would have been on the left side of the character.
+
+	return formattedString
+
 }
--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	myBill := newBill("Luffy's Bill")
-	fmt.Println(myBill)
+	fmt.Println(myBill.format())
 }
